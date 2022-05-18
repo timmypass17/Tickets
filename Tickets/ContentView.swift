426bd1ac18258f54dcf90122c980ca04a868aecd
Timmy Nguyen <timmypass99@gmail.com>
2022-05-17 20:39:16 -0700

    Initial Commit

---
+++ Tickets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tickets
+//
+//  Created by Timmy Nguyen on 5/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tickets/TicketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketsApp.swift
+//  Tickets
+//
+//  Created by Timmy Nguyen on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct TicketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
